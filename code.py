815daaab3619aea7b1551107e372bc381a7c2f9a
suman-19 <suman.kumar@innefu.com>
2021-01-23 19:24:25 +0530

    a new function divide() is added in code.py file

--- code.py
+++ code.py
@@ -9,8 +9,14 @@ def sub(a,b):
 def mul(a,b):
 	return a*b
 
+def divide(a,b):
+	if b == 0:
+		return 0
+	else:
+		return a/b
+
 if __name__ == "__main__":
 	add(1,3)
 	sub(3,1)
 	mul(1,5)
-
+	divide(10,2)
